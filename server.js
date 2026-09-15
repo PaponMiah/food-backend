c2a83d5263d3dc3ff5bf57a18a6feb2c1c81cc4b
Md Popon Miah <paponmiah646@gmail.com>
2026-09-15 18:43:15 +0200

    Create HTTP server for deployment

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-console.log("Food Backend Started");
-console.log("Server is running on port 5000");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Food Backend is running");
+});
+
+server.listen(5000, () => {
+    console.log("Food Backend Started");
+    console.log("Server is actually listening on port 5000");
+});
